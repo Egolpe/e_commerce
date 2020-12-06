6a7242db61134a8fda22fb41965251f00331a025
Enrique Golpe <enriquegolpe@hotmail.com>
2020-12-06 22:15:10 +0100

    Sign in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
 
 const HatsPage = () => (
@@ -21,6 +22,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
